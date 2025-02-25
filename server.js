07e14134687f881bd6681bfdc07309ffcd7b5911
pauly-johnson <paul.m.johnson1988@gmail.com>
2025-02-26 11:19:51 +1300

    f

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ const testRoutes = require('./routes/testRoutes');  // Import routes for CRUD op
 app.use('/api/test', testRoutes);  // Mount test routes under '/api/test'
 
 // Test route (API is working)
-// app.get('/', (req, res) => {
-//     res.send('API is working!');
-// });
+app.get('/', (req, res) => {
+    res.send('API is working!');
+});
 // app.get('/api/test', (req, res) => {
 //     res.json({ message: 'API is working!' });
 //   });
